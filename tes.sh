7ea88c7152f68ec3623f1844962cbb277e60b620
TheRebo <95853374+TheRebo@users.noreply.github.com>
2021-12-19 22:12:09 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,42 @@
+clear
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By Rebo
+echo $green"Selamat Datang"
+echo
+sleep 2
+echo $red"Pilih Tools-Nya :"
+echo
+echo $cyan"1.) Bye Bye Jaringan Lemot"
+echo
+echo $blue"0.) Pilih Ini Kalo Mau Keluar Tools"
+echo
+echo $white
+read -p "Pilih Mana? : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Rebo
+echo $cyan"Bye Bye Jaringan Lemot By Rebo"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "Byee..."
+echo $white"Script-Nya Masih Beta Oii"
+sleep 3
+exit
+fi
